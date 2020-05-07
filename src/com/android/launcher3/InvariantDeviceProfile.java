f5e1ae64024f9e0739891dd76bcb627efc2c27b7
DennySPb <dennyspb@gmail.com>
2020-05-07 13:07:31 +0300

    Add two lines app label option
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I8e5cc84e1ca61da268460c0a41127c3ec4b35a74

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -77,6 +77,8 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
 
     public static final String KEY_SHOW_DESKTOP_LABELS = "pref_desktop_show_labels";
     public static final String KEY_SHOW_DRAWER_LABELS = "pref_drawer_show_labels";
+    public static final String KEY_TWO_LINE_DESKTOP_LABELS = "pref_desktop_two_line_labels";
+    public static final String KEY_TWO_LINE_DRAWER_LABELS = "pref_drawer_two_line_labels";
     public static final String KEY_ICON_PATH_REF = "pref_icon_shape_path";
     public static final String KEY_WORKSPACE_EDIT = "pref_workspace_edit";
 
@@ -186,7 +188,8 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-        if (KEY_SHOW_DESKTOP_LABELS.equals(key) || KEY_SHOW_DRAWER_LABELS.equals(key)) {
+        if (KEY_SHOW_DESKTOP_LABELS.equals(key) || KEY_SHOW_DRAWER_LABELS.equals(key) ||
+              KEY_TWO_LINE_DRAWER_LABELS.equals(key) || KEY_TWO_LINE_DESKTOP_LABELS.equals(key)) {
             apply(mContext, CHANGE_FLAG_ICON_PARAMS);
         }
     }
