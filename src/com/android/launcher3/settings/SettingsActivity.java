5044e36321a699fe7060eef0990992bd30396ce7
Ali B <abittin@gmail.com>
2022-01-01 14:14:58 +0300

    Launcher3: restart launcher on DT option change
    Change-Id: I6d31d0053bde43683e7af5d4730776b35eb9b6ba
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -85,6 +85,7 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
 
     private static final String DEVELOPER_OPTIONS_KEY = "pref_developer_options";
     private static final String FLAGS_PREFERENCE_KEY = "flag_toggler";
+    private static final String DT2S_PREFERENCE_KEY = "pref_dt_gesture";
 
     private static final String NOTIFICATION_DOTS_PREFERENCE_KEY = "pref_icon_badging";
 
@@ -154,7 +155,15 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case DT2S_PREFERENCE_KEY:
+                LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                break;
+            default:
+                break;
+        }
+    }
 
 
     private boolean startPreference(String fragment, Bundle args, String key) {
