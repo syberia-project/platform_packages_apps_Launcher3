crdroidandroid/android_packages_apps_Launcher3

Launcher3: Trigger to hide navbar hint settings
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -57,8 +57,10 @@ import com.android.launcher3.util.IntArray;
 import com.android.launcher3.util.MainThreadInitializedObject;
 import com.android.launcher3.util.Themes;
 import com.android.launcher3.util.WindowBounds;
-
 import com.android.quickstep.SystemUiProxy;
+
+import lineageos.providers.LineageSettings;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -311,6 +313,9 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
                 // Create the illusion of this taking effect immediately
                 // Also needed because TaskbarManager inits before SystemUiProxy on start
                 boolean enabled = Utilities.getPrefs(mContext).getBoolean(DeviceProfile.KEY_PHONE_TASKBAR, isTablet);
+                LineageSettings.System.putInt(mContext.getContentResolver(),
+                        LineageSettings.System.ENABLE_TASKBAR, enabled ? 1 : 0);
+
                 SystemUiProxy.INSTANCE.get(mContext).setTaskbarEnabled(enabled);
 
                 onConfigChanged(mContext, true);

syberia-project/platform_packages_apps_Launcher3

Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: Ib7ce936e809aa273db5b936bf41949754fdb0169

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -56,8 +56,10 @@ import com.android.launcher3.util.IntArray;
 import com.android.launcher3.util.MainThreadInitializedObject;
 import com.android.launcher3.util.Themes;
 import com.android.launcher3.util.WindowBounds;
-
 import com.android.quickstep.SystemUiProxy;
+
+import android.providers.Settings;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -304,6 +306,9 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
                 // Create the illusion of this taking effect immediately
                 // Also needed because TaskbarManager inits before SystemUiProxy on start
                 boolean enabled = Utilities.getPrefs(mContext).getBoolean(DeviceProfile.KEY_PHONE_TASKBAR, false);
+                Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.ENABLE_TASKBAR, enabled ? 1 : 0);
+
                 SystemUiProxy.INSTANCE.get(mContext).setTaskbarEnabled(enabled);
 
                 onConfigChanged(mContext, true);