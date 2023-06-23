crdroidandroid/android_packages_apps_Launcher3

Launcher3: Fix error in TaskbarManager for registering shared prefs
* Registed after user is unlocked. Tested and works fine in all use cases.
* Fixes:
    java.lang.IllegalStateException: SharedPreferences in credential encrypted storage are not available until after user is unlocked

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -119,9 +119,6 @@ public class TaskbarManager implements OnSharedPreferenceChangeListener {
         mUserSetupCompleteListener = isUserSetupComplete -> recreateTaskbar();
         mNavBarKidsModeListener = isNavBarKidsMode -> recreateTaskbar();
 
-        SharedPreferences prefs = Utilities.getPrefs(mContext);
-        prefs.registerOnSharedPreferenceChangeListener(this);
-
         // TODO(b/227669780): Consolidate this w/ DisplayController callbacks
         mComponentCallbacks = new ComponentCallbacks() {
             private Configuration mOldConfig = mContext.getResources().getConfiguration();
@@ -221,6 +218,8 @@ public class TaskbarManager implements OnSharedPreferenceChangeListener {
      */
     public void onUserUnlocked() {
         mUserUnlocked = true;
+        SharedPreferences prefs = Utilities.getPrefs(mContext);
+        prefs.registerOnSharedPreferenceChangeListener(this);
         recreateTaskbar();
     }
 

syberia-project/platform_packages_apps_Launcher3

Signed-off-by: DennySPb <dennyspb@gmail.com>

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -147,9 +147,6 @@ public class TaskbarManager implements OnSharedPreferenceChangeListener {
         mUserSetupCompleteListener = isUserSetupComplete -> recreateTaskbar();
         mNavBarKidsModeListener = isNavBarKidsMode -> recreateTaskbar();
 
-        SharedPreferences prefs = LauncherPrefs.getPrefs(mContext);
-        prefs.registerOnSharedPreferenceChangeListener(this);
-
         // TODO(b/227669780): Consolidate this w/ DisplayController callbacks
         mComponentCallbacks = new ComponentCallbacks() {
             private Configuration mOldConfig = mContext.getResources().getConfiguration();
@@ -296,6 +293,8 @@ public class TaskbarManager implements OnSharedPreferenceChangeListener {
      */
     public void onUserUnlocked() {
         mUserUnlocked = true;
+        SharedPreferences prefs = LauncherPrefs.getPrefs(mContext);
+        prefs.registerOnSharedPreferenceChangeListener(this);
         recreateTaskbar();
     }
 