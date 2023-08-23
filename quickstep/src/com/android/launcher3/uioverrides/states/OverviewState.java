33677930c8a669cc930b5df2b1a9f8fe8b410697
nift4 <nift4@protonmail.com>
2023-08-23 15:58:29 +0200

    Launcher3: Show AOSP clear all button when chip disabled
    Change-Id: I96970593301a7577f2d71c908551b8ee06f43f0a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
@@ -27,6 +27,7 @@ import androidx.core.graphics.ColorUtils;
 
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.LauncherState;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
@@ -111,6 +112,7 @@ public class OverviewState extends LauncherState {
 
     @Override
     public int getVisibleElements(Launcher launcher) {
+        boolean clearAll = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_clear_all", true);
         int elements = OVERVIEW_ACTIONS;
         DeviceProfile dp = launcher.getDeviceProfile();
         boolean showFloatingSearch;
@@ -123,6 +125,9 @@ public class OverviewState extends LauncherState {
         }
         if (showFloatingSearch) {
             elements |= FLOATING_SEARCH_BAR;
+        }
+        if (!clearAll) {
+            elements |= CLEAR_ALL_BUTTON;
         }
         if (enableSplitContextual() && launcher.isSplitSelectionActive()) {
             elements &= ~CLEAR_ALL_BUTTON;
